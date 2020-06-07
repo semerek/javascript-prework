eef26e1cf106c333860e7560c3da456d995abf92
Marta Semrau <mm.semrau@gmail.com>
2020-06-07 15:37:46 +0200

    Rock paper scissors game

--- js/script.js
+++ js/script.js
@@ -1,7 +1,55 @@
 
-let computerMove = `kamień`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'papier';
-printMessage( 'Zagrałeś ' + playerMove + '! Mój ruch to ' + computerMove + ', wygrywasz!');
+let computerMove = 'randomNumber';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+  computerMove = 'papier';
+} else if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == '2'){
+  playerMove = 'papier';
+} else if  (playerInput == '3'){
+  playerMove ='nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+} else if( playerMove == 'kamień' && computerMove == 'papier'){
+  printMessage('Przegrałeś!');
+} else if( playerMove == 'papier' && computerMove == 'nożyce'){
+  printMessage('Przegrałeś!');
+} else if( playerMove == 'nożyce' && computerMove == 'kamień'){
+  printMessage('Przegrałeś!');
+} else if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+} else if( playerMove == 'nieznany ruch'){
+  printMessage('Nieprawidłowy ruch');
+}
