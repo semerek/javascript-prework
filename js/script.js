f62798e37aa357f5bedb89df6be538987e66a470
Marta Semrau <mm.semrau@gmail.com>
2020-06-07 10:16:12 +0200

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-let computerMove = `kamień`;
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
+console.log('Wylosowana liczba to: ' + randomNumber);
 
+let computerMove = 'nieznany ruch';
 
-let playerMove = 'papier';
+if(randomNumber == 1){
+  computerMove = 'kamień';
 
-printMessage('Zagrałeś ' + playerMove + '! Mój ruch to kamień,  wygrywasz!');
+} else if (randomNumber == 2){
+  computerMove = 'papier';
+
+} else (randomNumber == 3){
+  computerMove ='nożyce'
+}
+
+printMessage('Mój ruch to: ' + computerMove);
