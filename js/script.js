342f5df29ed19f1f86423c3f33398a225b0ca2a6
Marta Semrau <mm.semrau@gmail.com>
2020-06-07 14:51:39 +0200

    add corrections

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'randomNumber';
+let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
 } else if(randomNumber == 2){
-  computerMove = 'papier';
-} else if(randomNumber == 3){
+  computerMove ='papier';
+} else if(random == 3){
   computerMove = 'nożyce';
 }
 
@@ -25,8 +24,9 @@ if(playerInput == '1'){
   playerMove = 'kamień';
 } else if (playerInput == '2'){
   playerMove = 'papier';
-} else if  (playerInput == '3'){
-  playerMove ='nożyce';
+} else if (playerInput == '3'){
+  playerMove = 'nożyce';
+
 }
 
 printMessage('Twój ruch to: ' + playerMove);
